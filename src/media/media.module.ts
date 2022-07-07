68f51e5b8cfc735488e7dbecba08de3e4eaca33f
Mukhammadjon <kayumovmukhammadjon@gmail.com>
2022-07-07 23:11:32 +0500

    Adding category model

--- src/media/media.module.ts
+++ src/media/media.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { CommentModule } from './comment/comment.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports: [UserModule, CommentModule]
+  imports: [UserModule, CommentModule, CategoryModule]
 })
 export class MediaModule {}
