2650f404f7b600844e44b0b5c16906c4276ba420
Mukhammadjon <kayumovmukhammadjon@gmail.com>
2022-07-07 23:10:35 +0500

    Adding comment model

--- src/media/media.module.ts
+++ src/media/media.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { CommentModule } from './comment/comment.module';
 
 @Module({
-  imports: [UserModule]
+  imports: [UserModule, CommentModule]
 })
 export class MediaModule {}
